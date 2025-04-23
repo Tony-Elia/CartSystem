468c9167ac7fdbeef9a6f3a7f8385581852a10ae
Tony-Elia <antony.safaga@gmail.com>
2025-04-23 19:07:16 +0200

    base Models

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Cart extends Model
 {
-    //
+    protected $fillable = [
+        'product_id', 'user_id', 'quantity'
+    ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
